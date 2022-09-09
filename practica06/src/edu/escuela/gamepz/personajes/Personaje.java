ef9cc25179093612e0345c362f80e9989a5371da
Lspadachin <Lspadachin@gmail.com>
2022-09-09 07:45:02 -0500

    Practica06 07:44
    Paso 4

---
+++ practica06/src/edu/escuela/gamepz/personajes/Personaje.java
@@ -0,0 +1,35 @@
+package com.patito.poo1pract.personajes;
+public class Personaje {
+
+	private String nombre;
+	private int edad;
+
+	public Personaje(String personaje){
+		setNombre(personaje);
+		int edad = 0;
+		this.edad = edad;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+	public void Saludar() {
+		System.out.println("Hola Alumno de POO " + nombre);
+	}
+	public int getEdad() {
+		edad = this.edad;
+		return edad;
+	}
+	public boolean setEdad(int edad) {
+		if (0<edad & edad<120) {
+			this.edad = edad;
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
