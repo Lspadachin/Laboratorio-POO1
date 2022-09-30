12df6a45e203c52aa565600f53df648d24a2859d
Lspadachin <Lspadachin@gmail.com>
2022-09-30 07:53:03 -0500

    Parcial1 7:52

---
+++ Parcial1/src/mx/parc1/soft/pruebas/TestSeries.java
@@ -0,0 +1,27 @@
+package mx.parc1.soft.pruebas;
+
+public class TestSeries {
+
+    public static void main(String[] args) {
+
+        Serie s = new Serie();
+
+        int m = s.genNumero(100);
+
+        s.primos(m);
+
+        int o = s.primoN(m/10);
+
+        System.out.println(o);
+
+        int n = s.genNumero(100);
+
+        s.fibo(n);
+
+        int p = s.fiboN(3, 11);
+
+        System.out.println(p);
+
+    }
+
+}
