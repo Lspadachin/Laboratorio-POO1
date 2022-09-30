6765f6d81ce509d5181a5e71384c7904b42b07ed
Lspadachin <Lspadachin@gmail.com>
2022-09-30 08:33:17 -0500

    Parcial1 08:33

--- Parcial1/src/mx/parc1/soft/parcial/series/Serie.java
+++ Parcial1/src/mx/parc1/soft/parcial/series/Serie.java
@@ -5,6 +5,22 @@ public class Serie{
 		int n = (int)(Math.random()*numero*10);
 	} 
 	public int fiboN(int posicion){
+		if ((posicion == 1) | (posicion == 2)){
+			return 1;
+		}
+		int cont = 3;
+		int x = 1;
+		int y = 1;
+		int z = 1;
+		while (true){
+			x = y;
+			y = z;
+			z = x+y;
+			if (cont == posicion){
+				return z;
+			}
+			cont++;
+		}
 
 	}
 }
