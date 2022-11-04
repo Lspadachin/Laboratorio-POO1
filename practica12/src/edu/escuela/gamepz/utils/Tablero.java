5eface6ef74dca824a0c150da1846de6dbc6142c
Lspadachin <Lspadachin@gmail.com>
2022-11-04 09:00:36 -0600

    practica12 09:00

--- practica12/src/edu/escuela/gamepz/utils/Tablero.java
+++ practica12/src/edu/escuela/gamepz/utils/Tablero.java
@@ -2,14 +2,28 @@ package edu.escuela.gamepz.utils;
 import edu.escuela.gamepz.personajes.Personaje;
 public class Tablero{
 	public static final int MAX_SIZE = 10;
-	Personaje[] personajes = new Personaje[MAX_SIZE];
+	public static Personaje[] personajes = new Personaje[MAX_SIZE];
 
 	private Tablero(){}
 
 	public static void mostrar(){
+		int posicion = 1;
 		System.out.println("\nInicio * * * Contenido del arreglo en Tablero");
 		for (Personaje tmp : personajes ) {
+			if (tmp == null){
+				System.out.println(posicion + " - - - ");
+			} else{
+				System.out.println(""+posicion + tmp);
+			}
+			posicion +=1;
 			
 		}
+	}
+
+	public static void insertar(Personaje p, int pos) throws PersException{
+		if (pos < 0 || pos > MAX_SIZE){
+			throw new PersException("Incide fuera de rango ", pos);
+		}
+		personajes[pos] = p;
 	}
 }
