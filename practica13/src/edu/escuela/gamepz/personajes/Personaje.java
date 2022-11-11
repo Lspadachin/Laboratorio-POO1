17e0957168f621c755eb64f81270238123a8388e
Lspadachin <Lspadachin@gmail.com>
2022-11-11 09:48:17 -0600

    Practica13 09:48

---
+++ practica13/src/edu/escuela/gamepz/personajes/Personaje.java
@@ -0,0 +1,68 @@
+//Practica iniciada a las 7:35, practica finalizada 10:16
+package edu.escuela.gamepz.personajes;
+
+public abstract class Personaje implements Comparable<Personaje>{
+
+	private String nombre;
+	protected int vida;
+	private float size;
+
+	public Personaje(String nombre, int vida, float size){
+		if (size == 0.0f){
+			this.size = genSize();
+		} else {
+			this.size = Math.round(size)/100;
+		}
+		this.nombre = nombre;
+		setVida(vida);
+	}
+
+	public Personaje(String personaje){
+		size = 0.0f;
+		setNombre(personaje);
+		vida = 3;
+	}
+
+	public void setNombre(String nombre) {
+		int longitud = nombre.length();
+		if ((longitud > 3) && (longitud < 25)){
+			this.nombre = nombre;
+		}
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+	public void Saludar() {
+		System.out.println("Hola Alumno de POO " + nombre);
+	}
+	public int getVida() {
+		return vida;
+	}
+	public boolean setVida(int vida) {
+		if (0<vida && vida<100) {
+			this.vida = vida;
+			return true;
+		} else {
+			return false;
+		}
+	}
+	public String toString(){
+		return ""+nombre+"\t"+vida;
+	}
+
+	public float genSize(){
+		float  size = (float)(Math.random()*10);
+		DecimalFormat df = new DecimalFormat("#.00");
+		size = Float.valueOf(df.format(size));
+		return size;
+	}
+
+	public abstract void decVida();
+
+	public abstract void decVida(int vida);
+
+	public abstract void addVida();
+
+	public abstract void addVida(int vida);
+}
